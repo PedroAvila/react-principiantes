22a205d43f64d2105dd2994798e980dae0f803b3
PedroAvila <minimainor_austin@hotmail.com>
2021-05-24 09:50:07 -0500

    Tasks

--- src/App.js
+++ src/App.js
@@ -1,56 +1,21 @@
-import  React from 'react'
+import  React, { Component } from 'react'
 import './App.css'
+import tasks from './sample/tasks.json'
+import Tasks from './components/Tasks';
 
 
-class Helloworld extends React.Component {
+class App extends Component {
 
   state = {
-    show: true
+    tasks: tasks
   }
 
-  toggleShow = () => {
-    this.setState({ show: !this.state.show })
-  }
-
-  render() {
-
-    if (this.state.show) {
-      return(
-        <div id="hello">
-          <h3>{ this.props.subtitle }</h3>
-          { this.props.mytext }
-          <button onClick={ this.toggleShow }>Toggle Show</button>
-        </div>
-      )  
-    }else{
-      return <h1>
-              There are not elements
-              <button onClick={ this.toggleShow }>
-              Toggle Show
-              </button>
-            </h1>
-    }
-    
-  }
-}
-
-//const App = () => <div>This is my compoment: <Helloworld /></div>
-
-/* class App extends React.Component {
   render(){
-    return <div>This is my compoment: <Helloworld /></div>
+    return <div>
+      <Tasks tasks={ this.state.tasks } />
+    </div>
   }
-} */
-
-function App() {
-  return (
-    <div>
-      This is my component: 
-      <Helloworld mytext="Hello Fazt" subtitle="loremp ispum" /> 
-      <Helloworld mytext="hola mundo" subtitle="sdfsfsdf" /> 
-      <Helloworld mytext="Hello!" subtitle="component three" /> 
-      </div>
-  );
 }
+
 
 export default App;
